66995b8f53936362b5dec8bf8e445773468b1644
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-11-20 11:18:16 +0100

    initial test

--- src/DB_Zugriff.java
+++ src/DB_Zugriff.java
@@ -1,12 +1,12 @@
 import java.sql.*;
 public class DB_Zugriff {
 
-	String url = "localhost";
-	String database = "dbi";
-	String user = "root";
-	String pw = "root";
+	static String url = "localhost";
+	static String database = "dbi";
+	static String user = "root";
+	static String pw = "root";
 	
-	public void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException {
 		
 		Connect connector = new Connect();
 		System.out.println(connector.connectToDB(url, database, user, pw));
