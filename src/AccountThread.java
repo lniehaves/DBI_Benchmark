0d2ff1c28ab86af18f10998755e83796e5be971e
lniehaves <32952203+lniehaves@users.noreply.github.com>
2019-12-04 22:25:21 +0100

    mt beta

--- src/AccountThread.java
+++ src/AccountThread.java
@@ -2,24 +2,22 @@ import java.sql.*;
 
 public class AccountThread extends Thread{
 
-	String url = "localhost";
+	String url = "192.168.178.45";
 	String database = "dbi_benchmark";
 	String user = "root";
 	String pw = "root";
 	private int n = 10;
-	private int j;
-	int k;
+	private int oberGrenze;
+	private int unterGrenze;
 	
 	public AccountThread(int j, int k)
 	{
-		this.j=j;
-		this.k=k;
+		this.unterGrenze=j;
+		this.oberGrenze=k;
 	}
 	
 	public void run()
 	{
-		
-		Thread thread = Thread.currentThread();
 		try {
 			
 			Connect connector = new Connect();
@@ -27,12 +25,14 @@ public class AccountThread extends Thread{
 			Statement state = connector.connection.createStatement();
 			state.executeQuery("SET FOREIGN_KEY_CHECKS=0");
 			PreparedStatement pState = connector.connection.prepareStatement("Insert into accounts(accid,name,balance,branchid, address) VALUES(?,'aaaaaaaaaaaaaaaaaaaa',0,?,'tdduzstiduzsidgsdiucgoxyuiztcyxhgcoisazdioasgduzgasIUAgsiuaTSIUDatsi')");		
-			for(int i = j;i<=k;i++)
+			for(int i = unterGrenze;i<=oberGrenze;i++)
 			{
 				pState.setInt(1, i);
 				pState.setInt(2, (int)(Math.random()*n+1));
-				pState.executeQuery();
+				pState.executeUpdate();
+
 			}
+			
 			
 			
 		} catch (Exception e) {
--- src/DB_Zugriff.java
+++ src/DB_Zugriff.java
@@ -1,17 +1,13 @@
 import java.sql.*;
 public class DB_Zugriff {
 
-	static String url = "localhost";
+	static String url = "192.168.178.45";
 	static String database = "dbi_benchmark";
 	static String user = "root";
 	static String pw = "root";
-	static int n = 20;
-	static int threads = 10;
+	static int n = 10;
+	static int tLimit =12;
 	static Statement state;
-	
-	//Threads fuer account
-	
-	
 	
 	
 	public static void main(String[] args) throws SQLException {
@@ -35,28 +31,25 @@ public class DB_Zugriff {
 		
 		//starte befuellen
 		connector.connection.setAutoCommit(false);
-		PreparedStatement pState = connector.connection.prepareStatement("");
-		String sql = "";
-
+		PreparedStatement pStateBranches = connector.connection.prepareStatement("Insert into branches(branchid,branchname,balance,address) VALUES(?,'abcdefghijklmnopqrst',0,'tdduzstiduzsidgsdiucgoxyuiztcyxhgcoisazdioasgduzgasIUAgsiuaTSIUDatsiudza')");
+		PreparedStatement pStateTellers = connector.connection.prepareStatement("Insert into tellers(tellerid,tellername,balance,branchid, address) VALUES(?,'taaaaaaaaaaaaaaaaaaa',0,?,'tdduzstiduzsidgsdiucgoxyuiztcyxhgcoisazdioasgduzgasIUAgsiuaTSIUDatsi')");
+		state.executeQuery("SET FOREIGN_KEY_CHECKS=0");
 		
 		//Zeit neu initialisieren
 		t1 = System.currentTimeMillis();
-		//Branches
+		
+		
+		//Branches befüllen
 		for(int i = 1; i<=n; i++)
 		{
-
-			pState = connector.connection.prepareStatement("Insert into branches(branchid,branchname,balance,address) VALUES(?,'abcdefghijklmnopqrst',0,'tdduzstiduzsidgsdiucgoxyuiztcyxhgcoisazdioasgduzgasIUAgsiuaTSIUDatsiudza')");
-			pState.setInt(1,i);
-			pState.addBatch();
+			pStateBranches.setInt(1,i);
+			pStateBranches.addBatch();
 		}
-		
-
+		pStateBranches.executeBatch();
 		System.out.println("Branches Befüllt: "+ (System.currentTimeMillis()-t1)+" ms");
 		
 		
-		//Accounts
-		state.executeQuery("SET FOREIGN_KEY_CHECKS=0");
-		
+		//Accounts befüllen	
 		try {
 			AccountThread aT1 = new AccountThread(1,125000);
 			AccountThread aT2 = new AccountThread(125001,250000);
@@ -65,10 +58,7 @@ public class DB_Zugriff {
 			AccountThread aT5 = new AccountThread(500001,625000);
 			AccountThread aT6 = new AccountThread(625001,750000);
 			AccountThread aT7 = new AccountThread(750001,875000);
-			AccountThread aT8 = new AccountThread(875001,1000000);
-			
-			
-			state.executeQuery("SET FOREIGN_KEY_CHECKS=0");
+			AccountThread aT8 = new AccountThread(875001,1000000);			
 
 			aT1.start();
 			aT2.start();
@@ -79,39 +69,37 @@ public class DB_Zugriff {
 			aT7.start();
 			aT8.start();
 			
+		
+			
+
+			
+			
+
+			System.out.println("Alle Tabellen Befüllt Befüllt: "+ (System.currentTimeMillis()-t1)+" ms");
+			
 		}
 		catch (Exception e) {
 			e.printStackTrace(); 
 		}
-		
-		
-
-
 		System.out.println("Accounts Befüllt: "+ (System.currentTimeMillis()-t1)+" ms");
 	
-		//Teller
-
-		
-
-
-		/*
-		for(int i = 1; i<=n*100000; i++)
+		//Teller befüllen
+		for(int i = 1; i<=n*10; i++)
 		{
-
+			int zahl = (int)(Math.random()*n+1);
+			pStateTellers.setInt(2, (int)(Math.random()*n+1)); 
+			pStateTellers.setInt(1,i);
+			pStateTellers.addBatch();
 		}
+		pStateTellers.executeBatch();
 		System.out.println("Tellers Befüllt: "+ (System.currentTimeMillis()-t1)+" ms");
-		*/
-		
-		System.out.println("Alle Batches executet "+ (System.currentTimeMillis()-t1)+" ms");
+
+	
 		//batches commiten
 
 		connector.connection.commit();
-
 		
 		
-		//history
-		
-		System.out.println("Alle Tabellen Befüllt Befüllt: "+ (System.currentTimeMillis()-t1)+" ms");
 				
 		
 		
